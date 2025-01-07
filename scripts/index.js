28b0dd401badfee4656f7f78b525636cf36380f6
Ana Cris 1125 <amejialalinde@hotmail.com>
2025-01-07 10:23:46 +0800

    Nueva actualizacion de mi proyecto

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,171 @@
-let editButton = document.querySelector("#edit-button");
-let saveButton = document.querySelector(".popup__save-button");
-let closeButton = document.querySelector("#close-button");
-let formElement = document.querySelector(".popup__form");
+const page = document.querySelector(".page");
 
-let popup = document.querySelector(".popup");
+const editButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__info-name");
+const profileAbout = document.querySelector(".profile__info-ocupation");
 
-const nameInput = document.querySelector("#name");
-const aboutInput = document.querySelector("#about");
+const cardsContainer = document.querySelector(".cards__container");
+const cardTemplate = document.querySelector("#card-template").content;
+const profileButton = document.querySelector(".profile__add-button");
 
-const userName = document.querySelector(".profile__info-name");
-const userAbout = document.querySelector(".profile__info-ocupation");
+const popup = document.querySelector(".popup");
+const closePopUpButton = document.querySelector(".popup-close");
+const firstInput = document.querySelector(".popup__form-first");
+const secondInput = document.querySelector(".popup__form-second");
+const formElement = document.querySelector(".popup__form");
+const popupFormTitle = document.querySelector(".popup__form-title");
+const popupFormButton = document.querySelector(".popup__form-button");
 
-function openPopup() {
-  initForm();
+const bigImage = document.querySelector(".bigimage");
+const bigImageIlustration = document.querySelector(".bigimage-ils");
+const bigImageTitle = document.querySelector(".bigimage-title");
+const closeBigImage = document.querySelector(".bigimage-close");
+
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+  },
+];
+
+// create cards
+
+initialCards.forEach((card) => {
+  let cardTemplate = createCard(card);
+  cardsContainer.prepend(cardTemplate);
+}
+);
+
+function toggleOpenBigImage(evt) {
+  bigImage.classList.toggle("bigimage__opened");
+  bigImageIlustration.src = evt.target.src;
+  bigImageTitle.innerText = evt.target.name;
+}
+
+
+function createCard(card) {
+  let cardElm = cardTemplate.querySelector(".cards__item").cloneNode(true);
+  cardElm.querySelector(".cards__item-img").src = card.link;
+  cardElm.querySelector(".cards__item-name").textContent = card.name;
+
+  deleteItemTrashButton(cardElm);
+  toggleItemHeartButton(cardElm);
+
+  const image = cardElm.querySelector(".cards__item-img");
+  const name = cardElm.querySelector(".cards__item-name");
+  image.addEventListener("click", toggleOpenBigImage);  
+  name.addEventListener("click", toggleOpenBigImage);
+ 
+
+  return cardElm;
+}
+
+function deleteItemTrashButton(card) {
+  let deleteButton = card.querySelector(".cards__item-delete");
+  deleteButton.addEventListener("click", function () {
+    card.remove();
+  });
+}
+
+function toggleItemHeartButton(card) {
+  let heartButton = card.querySelector(".cards__item-like");
+  heartButton.addEventListener("click", function () {
+    heartButton.classList.toggle("cards__item-like_active");
+  });
+}
+
+function openEditAddPopup(evt) {
+  const className = evt.currentTarget.className;
+
+  switch (className) {
+    case "profile__edit-button":
+      popupFormTitle.textContent = "Editar perfil";
+      popupFormButton.textContent = "Guardar";
+      firstInput.placeholder = "Nombre";
+      secondInput.placeholder = "Ocupación";
+      firstInput.value = profileName.textContent;
+      secondInput.value = profileAbout.textContent;
+      break;
+    case "profile__add-button":
+      popupFormTitle.textContent = "Nuevo lugar";
+      popupFormButton.textContent = "Agregar";
+      firstInput.placeholder = "Lugar";
+      secondInput.placeholder = "Enlace a la imagen";
+      break;
+  }
+
   popup.classList.add("popup__opened");
 }
 
-function closePopup() {
-  popup.classList.remove("popup__opened");
-}
+function formSubmit(evt) {
+  evt.preventDefault()
+  const buttonText = evt.target[2].innerText;
 
-function initForm() {
-  nameInput.value = userName.textContent;
-  aboutInput.value = userAbout.textContent;
-}
-
-function updateProfile() {
-  userName.textContent = nameInput.value;
-  userAbout.textContent = aboutInput.value;
-}
-
-function cleanInputs() {
-  nameInput.value = "";
-  aboutInput.value = "";
-}
-
-function listenInputs() {
-  if (nameInput.value.trim() === "" || aboutInput.value.trim() === "") {
-    saveButton.disabled = true;
-  } else {
-    saveButton.disabled = false;
+  switch (buttonText) {
+    case "Guardar":
+      profileName.textContent = firstInput.value;
+      profileAbout.textContent = secondInput.value;
+      firstInput.value = "";
+      secondInput.value = "";
+      break;
+    case "Agregar":
+      const card = {
+        name: firstInput.value,
+        link: secondInput.value,
+      }
+      let cardElme = createCard(card);
+      cardsContainer.prepend(cardElme);
+      break;
   }
+  popup.classList.remove("popup__opened");
+
 }
 
-function saveData(evt) {
-  evt.preventDefault();
-  updateProfile();
-  cleanInputs();
-  closePopup();
+function formBigImageClose(evt) {
+  const className = evt.currentTarget.className;
+
+  switch (className) {
+    case "popup-close":
+      
+      break;
+    case "bigimage-close":
+      
+      break;
+  }
+  
+  popup.classList.remove("popup__opened");
+  bigImage.classList.remove("bigimage__opened");
 }
 
-// // // Eventos
-editButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
-formElement.addEventListener("submit", saveData);
-formElement.addEventListener("input", listenInputs);
+
+
+editButton.addEventListener("click", openEditAddPopup);
+profileButton.addEventListener("click", openEditAddPopup);
+formElement.addEventListener("submit", formSubmit);
+closePopUpButton.addEventListener("click", formBigImageClose);
+closeBigImage.addEventListener("click", formBigImageClose);
+
+
+
+
